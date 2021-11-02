161e599f6ea616daec492bf92b81077939933e54
Roberto <robertoriveraterangmail.com>
2021-11-01 21:44:16 -0500

    Firebase Auth code (Not yet functional)

--- HealthApp/LoginViewController.swift
+++ HealthApp/LoginViewController.swift
@@ -23,6 +23,30 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func logInButtonAction(_ sender: Any) {
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            
+            Auth.auth().signIn(withEmail: email, password: password){
+                (result, error) in
+                
+                if let result = result, error == nil {
+                    
+                    //self.navigationController?
+                        //.pushViewController(HomeViewController(email:
+                        //result.user.email!, provider: .basic), animated: true)
+                    
+                }else{
+                    
+                    let alertController = UIAlertController(title: "Error",
+                        message:
+                            "Se produjo un error en el Login", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                    
+                }
+            }
+        }
     }
     /*
     // MARK: - Navigation
