ef5643da2b0c7706b24ec1ed489443997f4581c7
Alexis Monroy <A01026848@itesm.mx>
2021-11-03 22:01:14 -0500

    Adelanto del fetch de reports

--- HealthApp/Reports/Reports.swift
+++ HealthApp/Reports/Reports.swift
@@ -6,15 +6,18 @@
 //
 
 import Foundation
+import Firebase
 
 // MARK: - Welcome
 struct Report: Codable {
+        var id:String
 		let nombre, nombreVacuna, fechaUltimaVacuna: String
 		let numeroDosis: Int
 		let dolorCabeza, fiebre, cuerpoCortado, congestionNasal: Bool
 		let otro: String
 
 		enum CodingKeys: String, CodingKey {
+                case id = "id"
 				case nombre = "nombre"
 				case nombreVacuna = "nombre_vacuna"
 				case fechaUltimaVacuna = "fecha_ultima_vacuna"
@@ -37,6 +40,20 @@ struct Report: Codable {
         self.cuerpoCortado = cuerpoCortado
         self.congestionNasal = congestionNasal
         self.otro = otro
+        self.id = "1234"
+    }
+    
+    init(d:DocumentSnapshot){
+        self.id = d.documentID
+        self.nombre = d.get("nombre") as? String ?? ""
+        self.nombreVacuna = d.get("nombre_vacuna") as? String ?? ""
+        self.fechaUltimaVacuna = d.get("fecha_utima_vacuna") as? String ?? ""
+        self.numeroDosis = d.get("numero_dosis") as? Int ?? 0
+        self.dolorCabeza = d.get("dolor_cabeza") as? Bool ?? false
+        self.fiebre = d.get("fiebre") as? Bool ?? false
+        self.cuerpoCortado = d.get("cuerpo_cortado") as? Bool ?? false
+        self.congestionNasal = d.get("congestion_nasal") as? Bool ?? false
+        self.otro = d.get("otro") as? String ?? ""
     }
 }
 
--- HealthApp/Reports/ReportsController.swift
+++ HealthApp/Reports/ReportsController.swift
@@ -6,32 +6,52 @@
 //
 
 import Foundation
+import Firebase
 
 class ReportsController {
-	func fetchReports(completion: @escaping (Result<Reports,Error>)->Void){
-		
-    let urlComponents = URLComponents(string: "http://martinmolina.com.mx/202113/tc2024/equipo1/formulario.json")!
+    let db = Firestore.firestore()
+//	func fetchReports(completion: @escaping (Result<Reports,Error>)->Void){
+//
+//    let urlComponents = URLComponents(string: "http://martinmolina.com.mx/202113/tc2024/equipo1/formulario.json")!
+//
+//	let task = URLSession.shared.dataTask(with: urlComponents.url!) { (data, response, error) in
+//			let jsonDecoder = JSONDecoder()
+//			if let data = data{
+//					do{
+//							let reports = try? jsonDecoder.decode([Report].self, from: data)
+//							completion(.success(reports!))
+//                        print("---------------------------------------------------------------------")
+//						print(reports!)
+//					}
+//					catch{
+//							completion(.failure(error))
+//					}
+//			}
+//			else {
+//					completion(.failure(error!))
+//			}
+//
+//	}
 
-	let task = URLSession.shared.dataTask(with: urlComponents.url!) { (data, response, error) in
-			let jsonDecoder = JSONDecoder()
-			if let data = data{
-					do{
-							let reports = try? jsonDecoder.decode([Report].self, from: data)
-							completion(.success(reports!))
-                        print("---------------------------------------------------------------------")
-						print(reports!)
-					}
-					catch{
-							completion(.failure(error))
-					}
-			}
-			else {
-					completion(.failure(error!))
-			}
-			
-	}
+//	task.resume()
+//
+//}
 
-	task.resume()
-	
-}
+    
+    func fetchReports(completion: @escaping (Result<Reports,Error>)->Void){
+        var lista_reportes = [Report]()
+        
+        db.collection("reportes").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+                completion(.failure(err))
+            } else {
+                for document in querySnapshot!.documents {
+                    var r = Report(d: document)
+                    lista_reportes.append(r)
+                }
+                completion(.success(lista_reportes))
+            }
+        }
+    }
 }
