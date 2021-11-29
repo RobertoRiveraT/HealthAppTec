3bbdfbc34ab8537dc1f36acc89ee38e260596d2e
Roberto <robertoriveraterangmail.com>
2021-11-29 12:07:32 -0600

    added WatchKit

---
+++ HealthAppAW WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  HealthAppAW WatchKit Extension
+//
+//  Created by user189674 on 11/29/21.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
