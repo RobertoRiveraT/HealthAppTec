28c2d045d9546988c58c960d393e5a7374911312
Roberto <robertoriveraterangmail.com>
2021-11-30 16:38:46 -0600

    Read name from database

--- HealthApp/ProfileViewController.swift
+++ HealthApp/ProfileViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var NombreLabel: UILabel!
+    
+    var ref: DatabaseReference!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        guard let userID = Auth.auth().currentUser?.uid else { return }
+        self.ref = Database.database().reference()
+        
+        //Leer
+        self.ref.child(userID).observeSingleEvent(of: .value, with: { snapshot in
+            guard let value = snapshot.value as? [String: Any] else {
+                return
+            }
+            
+            self.NombreLabel.text = value["Nombre"] as! String
+            
+            print("Readed value: \(value)")
+            print("Readed value first: \(value["Nombre"])")
+        })
+        
     }
+    
     
 
     /*
